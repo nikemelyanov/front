a5684d0f9931ba2b90b8cfec35c7cd0be8777a21
Nikita Emelyanov <emelyanovtime@gmail.com>
2023-08-01 00:18:59 +0300

    firstCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import PrivateRoute from './utils/router/PrivateRoute';
+import Home from './pages/home';
+import Login from './pages/auth/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path='/' element={<Home />}/>
+        </Route>
+
+        <Route path='login' element={<Login />}/>
+      </Routes>
     </div>
   );
 }
