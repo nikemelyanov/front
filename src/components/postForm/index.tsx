46e6e3ecdfde00b502d10e1f21761b249afb7d4d
nikemelyanov <emelyanovtime@gmail.com>
2023-08-10 19:12:19 +0300

    user post update

--- src/components/postForm/index.tsx
+++ src/components/postForm/index.tsx
@@ -9,11 +9,21 @@ export default function PostForm() {
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
+    const token = localStorage.getItem('token')
+    console.log(token)
     axios
-      .post('http://localhost:4000/posts/addPost', {
-        title: title,
-        body: body,
-      })
+      .post(
+        'http://localhost:4000/posts/addPost',
+        {
+          title: title,
+          body: body,
+        },
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        }
+      )
       .catch((error) => {
         console.error(error);
       });
@@ -21,8 +31,20 @@ export default function PostForm() {
 
   return (
     <form className={styles.postForm} onSubmit={handleSubmit}>
-      <input className={styles.postTitle} type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      <input className={styles.postBody} type="text" placeholder="body" value={body} onChange={(e) => setBody(e.target.value)} />
+      <input
+        className={styles.postTitle}
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        className={styles.postBody}
+        type="text"
+        placeholder="body"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
       <button type="submit">опубликовать</button>
     </form>
   );
